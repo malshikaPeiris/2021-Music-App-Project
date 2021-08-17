62fd69504c99416fe5a0778d1e8aeecd0a236bb4
malshika <gpmalshika@gmail.com>
2021-08-17 12:56:03 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/samplemusicapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.samplemusicapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnStart, btnStop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnStart = findViewById(R.id.btnStart);
+        btnStop = findViewById(R.id.btnStop);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                startService(new Intent(MainActivity.this, MyService.class));
+            }
+        });
+        btnStop.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                stopService(new Intent(MainActivity.this, MyService.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/samplemusicapp/MyService.java
@@ -0,0 +1,43 @@
+package com.example.samplemusicapp;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.provider.Settings;
+
+import androidx.annotation.Nullable;
+
+public class MyService extends Service {
+
+    private MediaPlayer player;
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        //getting systems default ringtone
+        player = MediaPlayer.create(this,
+                Settings.System.DEFAULT_RINGTONE_URI);
+        //setting loop play to true
+        //this will make the ringtone continuously playing
+        player.setLooping(true);
+
+        //staring the player
+        player.start();
+
+        //we have some options for service
+        //start sticky means service will be explicity started and stopped
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        //stopping the player when service is destroyed
+        player.stop();
+    }
+}
